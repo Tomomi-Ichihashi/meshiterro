192745597e51e629bfa9852ae22797e470e60939
Tomomi-Ichihashi <ichihashi.1414.tomomi@gmail.com>
2021-03-11 07:36:40 +0000

    [Add]index&show

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'e5bd197cb5cbce16a917f54d27c8cbc9c13dcc07ff96d3ac3a7560f9a26f3d35f03d71c8891728e2085893f37f8c4a646f4d11d43fc629a3a48bc65b36b78749'
