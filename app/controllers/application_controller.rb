15f287d1b53595c7bfb9e18268fbdd3fcf093f91
Tomomi-Ichihashi <ichihashi.1414.tomomi@gmail.com>
2021-03-13 05:03:52 +0000

    [Start]4-6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :authenicate_user!,except: [:top]
+  
   before_action :configure_permitted_parameters, if: :devise_controller?
   
   def after_sign_in_path_for(resource)
